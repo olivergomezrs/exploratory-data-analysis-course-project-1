68ca790f329f4e93d6bd4694e7805e146446d0c8
Oliver Gómez <90093706+olivergomezrs@users.noreply.github.com>
2024-05-10 19:15:55 -0600

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -17,5 +17,6 @@ with(subpower, lines(Time, as.numeric(as.character(Sub_metering_2)), col="red"))
 with(subpower, lines(Time, as.numeric(as.character(Sub_metering_3)), col="blue"))
 legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
+
 # Titulo
 title(main="Energy sub-metering")
